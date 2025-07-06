0edadcdcc651137b49a92f30f3688d4f607a8fb7
WelintonJunior <welintonjr011@gmail.com>
2025-07-06 19:23:56 -0300

    Adjust in Dtc log structure

--- types/dtcLog.go
+++ types/dtcLog.go
@@ -2,12 +2,18 @@ package types
 
 import "time"
 
+const (
+	StatusAtivo     = "ativo"
+	StatusResolvido = "resolvido"
+	StatusApagado   = "apagado"
+)
+
 type DTCLog struct {
 	Base
 
 	TimestampDetected time.Time  `gorm:"not null" json:"timestamp_detected"`
 	DTCCode           string     `gorm:"not null" json:"dtc_code"`
 	Description       string     `json:"description"`
-	Status            string     `gorm:"type:enum('ativo','resolvido','apagado');default:'ativo'" json:"status"`
-	ClearedAt         *time.Time `json:"cleared_at,omitempty"` // pode ser nulo
+	Status            string     `gorm:"type:text;default:'ativo'" json:"status"`
+	ClearedAt         *time.Time `json:"cleared_at,omitempty"`
 }
